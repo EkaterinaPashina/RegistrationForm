546bcbdce8cdeedee6ebbef15dd65522c589a437
EkaterinaPashina <k377at@mail.ru>
2023-06-30 15:00:21 +0500

    added html, css

---
+++ js.js
@@ -0,0 +1,10 @@
+const form = document.forms.user;
+
+// function checkValidity(input) {
+//     let validity = input.validity;
+// }
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+});
