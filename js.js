8ea466fcbba902c8cd2ea1ba0a70982302dd0d37
EkaterinaPashina <k377at@mail.ru>
2023-07-01 19:33:05 +0500

    added js

--- js.js
+++ js.js
@@ -1,10 +1,16 @@
 const form = document.forms.user;
-
-// function checkValidity(input) {
-//     let validity = input.validity;
-// }
+let inputs = document.querySelectorAll("input");
+let selects = document.querySelectorAll("select");
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+    for (let input of inputs) {
+        let inputid = input.id;
+        document.getElementById(`${inputid}-error`).innerText = input.validationMessage;
+    }
+    for (select of selects) {
+        let selectid = select.id;
+        document.getElementById(`${selectid}-error`).innerText = select.validationMessage;
+    }
 
 });
