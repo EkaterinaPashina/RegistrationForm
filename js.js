5836172a651db45e6d2c75a5c749e199b8a12000
EkaterinaPashina <k377at@mail.ru>
2023-07-03 17:57:20 +0500

    added eventlistener to js

--- js.js
+++ js.js
@@ -1,59 +1,85 @@
-const form = document.forms.user;
-
-function checkCheckbox() {
-    const manCB = document.getElementById("man");
-    const womanCB = document.getElementById("woman");
-    if (!manCB.checked && !womanCB.checked) {
-        document.getElementById("sex-error").innerText = "Выберите ваш пол.";
-    } else {
-        console.log(user[3].value);
-    }
-};
-
-const select = document.querySelector('select');
-
-function checkSelect() {
-    if (select.value === "") {
-        document.getElementById("profession-error").innerText = "Выберите вашу профессию.";
-    } else {
-        console.log(select.value);
-    }
-};
-
-const age = document.getElementById('age');
-
-function checkAge() {
-    if (age.value.length === 0) {
-        document.getElementById("age-error").innerText = "Введите ваш возраст.";
-    } else if (age.value > 100) {
-        document.getElementById("age-error").innerText = "Вы указали слишком большой возраст.";
-    }
-};
-
 const password = document.getElementById('password');
 const passwordRepeat = document.getElementById('passwordRepeat');
 
 function checkPasswords() {
-    if (password.value === passwordRepeat.value) {
-        console.log(password.value);
-    } else {
+    if (!password.value === passwordRepeat.value) {
         document.getElementById("passwordRepeat-error").innerText = "Пароли не совпадают.";
+    } else {
+        document.getElementById("passwordRepeat-error").innerText = '';
     }
-}
+};
 
-let inputs = document.querySelectorAll(".errored-message");
+const form = document.forms.user;
+const arrayForms = Array.from(form.elements);
+arrayForms.forEach((element) => {
+    const parent = element.parentNode;
+    const errorDiv = parent.querySelector('.error-message');
+    errorDiv.innerHTML += element.validationMessage;
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+    element.addEventListener('change', () => {
+        let validity = element.validity;
+        if (validity.valueMissing) {
+            document.getElementById(`${element.id}-error`).innerText = element.validationMessage;
+        } else {
+            document.getElementById(`${element.id}-error`).innerText = '';
+        }
 
-    checkCheckbox();
-    checkSelect();
-    checkAge();
+        if (validity.patternMismatch) {
+            document.getElementById(`${element.id}-error`).innerText = element.validationMessage;
+        } else {
+            document.getElementById(`${element.id}-error`).innerText = '';
+        }
 
-    for (let input of inputs) {
-        let inputId = input.id;
-        document.getElementById(`${inputId}-error`).innerText = input.validationMessage;
-        console.log(input.value);
+        if (validity.rangeOverflow) {
+            document.getElementById(`${element.id}-error`).innerText = element.validationMessage;
+        } else {
+            document.getElementById(`${element.id}-error`).innerText = '';
+        }
+
+        if (validity.rangeUnderflow) {
+            document.getElementById(`${element.id}-error`).innerText = element.validationMessage;
+        } else {
+            document.getElementById(`${element.id}-error`).innerText = '';
+        }
+
+        if (validity.tooLong) {
+            document.getElementById(`${element.id}-error`).innerText = element.validationMessage;
+        } else {
+            document.getElementById(`${element.id}-error`).innerText = '';
+        }
+
+        if (validity.tooShort) {
+            document.getElementById(`${element.id}-error`).innerText = element.validationMessage;
+        } else {
+            document.getElementById(`${element.id}-error`).innerText = '';
+        }
+        if (form.elements.email.getAttribute('type') === "email") {
+            const mailFormat = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+            if (form.elements.email.value === "") {
+                document.getElementById("email-error").innerText = 'Заполните это поле.';
+            } else
+            if (!mailFormat.test(form.elements.email.value)) {
+                document.getElementById("email-error").innerText = 'Введите email в правильном формате';
+            } else {
+                document.getElementById("email-error").innerHTML = '';
+            }
+        }
+        if (form.elements.email.getAttribute('type') === "number") {
+            console.log(form.elements.age.value);
+            if (form.elements.age.value === "") {
+                document.getElementById("email-error").innerText = 'Заполните это поле.';
+            } else if (Number(form.elements.age.value) < 1) {
+                document.getElementById("email-error").innerText = 'Введите значение больше 1';
+            } else {
+                document.getElementById("email-error").innerHTML = '';
+            }
+        }
+    })
+    if (element.validity) {
+        console.log(element.value);
     }
+});
+
+form.addEventListener('input', (event) => {
     checkPasswords();
 });
